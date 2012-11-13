24373d8509617f2c8b82d0c248af4ca1ebdaa321
jwoz <wozniak.jan@gmail.com>
2012-11-13 18:58:00 +0100

    some updates :-)

--- proto/src/client.rb
+++ proto/src/client.rb
@@ -3,16 +3,34 @@ load 'rtp_interface.rb'
 
 iface = [] 
 iface << Interface.new(:main, "127.0.0.1", 0)
+msg = "message"
+
+remote_address = "127.0.0.1"
+remote_port = 5060
+
+local_port = 6666
 
 begin
     str = gets.chomp
-    x, p1, msg, p2 = str.split(' ')
-    local_port = p1.to_i
-    remote_port = p2.to_i
+    p0, p1, p2, p3, p4 = str.split(' ')
+    x = p0
+
     if x == 'b'
-        iface[0].new_connection(local_port, "127.0.0.1", local_port)
+        local_port = p1.to_i if p1
+        iface[0].new_connection(local_port, remote_address, local_port)
     elsif x == 's'
-        iface[0].send_msg(local_port, msg, "127.0.0.1", remote_port)
+        msg = p1 if p1
+        remote_address = p2 if p2
+        remote_port = p3.to_i if p3
+        local_port = p4.to_i if p4
+        iface[0].send_msg(local_port, msg, remote_address, remote_port)
+    elsif x == 'sp'
+        Thread.new{
+            (0..500).each {|x|
+                iface[0].send_msg(local_port, msg, remote_address, remote_port)
+                sleep 0.02
+            }
+        }
     end
 end until !x
 
