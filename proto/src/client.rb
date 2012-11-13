dd24ccaa41613d28165ed2459fb191c9fa1a844e
jwoz <wozniak.jan@gmail.com>
2012-11-13 17:42:14 +0100

    added missing files

---
+++ proto/src/client.rb
@@ -0,0 +1,19 @@
+require 'io/console'
+load 'rtp_interface.rb'
+
+iface = [] 
+iface << Interface.new(:main, "127.0.0.1", 0)
+
+begin
+    str = gets.chomp
+    x, p1, msg, p2 = str.split(' ')
+    local_port = p1.to_i
+    remote_port = p2.to_i
+    if x == 'b'
+        iface[0].new_connection(local_port, "127.0.0.1", local_port)
+    elsif x == 's'
+        iface[0].send_msg(local_port, msg, "127.0.0.1", remote_port)
+    end
+end until !x
+
+iface.each {|i| i.close_interface}
