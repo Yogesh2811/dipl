7bd3a166ff1e0ae4f47695393d7157820b84dd14
jwoz <wozniak.jan@gmail.com>
2013-02-25 15:28:14 +0100

    ruby measure

--- proto/src/client.rb
+++ proto/src/client.rb
@@ -1,7 +1,56 @@
 require 'socket'
 
-msg = IO.binread("packets/1")
-s = UDPSocket.new(Socket::AF_INET6)
-s.send(msg, 0, "::1", 16000)
-p s.recvfrom(10)
+@payload="\xfa\xfa\xfa\xfa\xfa\xfa\xfa\xfa\xfa\xfa\xfa\xfa\xfa"+
+         "\xfa\xfa\xfa"
 
+@t1 = Array.new
+@t2 = Array.new
+
+class Time
+    def to_ms
+        1000.0 * self.to_f
+    end
+end
+
+def make_packet(seq, timestamp, ssrc)
+    seq1 = seq >> 8
+    seq2 = seq & 0xff
+    pck = String.new
+    pck << "\x80" #v, p, e, csrc count
+    pck << "\x09" #marker, codec
+    pck << seq1 #seq 1.byte
+    pck << seq2 #seq 2.byte
+    pck << timestamp
+    pck << ssrc
+    pck << @payload
+    return pck
+end
+
+def measure_start(s, seq)
+    msg = make_packet(seq, "\xff\xff\xff\xff",
+                      "\x45\x92\x29\x67")
+    t = Time.now.to_ms
+    @t1[seq] = t
+    s.send(msg, 0, "127.0.0.1", 16000)
+end
+
+def measure_stop(s)
+    packet = s.recvfrom(1024)
+    t = Time.now.to_ms
+    seq = packet[0].to_i
+    @t2[seq] = t
+end
+
+#s = UDPSocket.new(Socket::AF_INET6)
+#s.send(msg, 0, "::1", 16000)
+
+s = UDPSocket.new(Socket::AF_INET)
+
+for i in 0..10
+    measure_start(s, i)
+    measure_stop(s)
+end
+
+@t1.each_with_index { |t1, seq|
+    puts "#{@t2[seq]-t1}"
+}
--- proto/src/gateway.rb
+++ proto/src/gateway.rb
@@ -1,12 +1,23 @@
-require 'io/console'
-load 'rtp_interface.rb'
+require 'socket'
 
-iface = Interface.new(:sip, "127.0.0.1", 5060)
+def parse_packet(packet)
+    seq1,seq2 = packet[2].ord, packet[3].ord
+    seq = (seq1<<8) | (seq2&0xff)  
+    return seq.to_s
+end
 
-begin
-    str = gets.chomp 
-    x, y = str.split(' ')
-end until !x
+#s = UDPSocket.new(Socket::AF_INET6)
+#s.bind("::1",16000)
+s = UDPSocket.new(Socket::AF_INET)
+s.bind("127.0.0.1",16000)
 
-iface.close_interface
+while true do
+    packet = s.recvfrom(1024)
+    remote_address = packet[1][2].to_s
+    remote_port = packet[1][1].to_i
+    response = parse_packet(packet[0])
+    #sleep(0.001)
+    puts response
+    s.send(response, 0, remote_address, remote_port)
+end
 
